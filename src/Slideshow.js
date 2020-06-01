133ad6af16090f449fc4ebb6b433f14597055ea9
Barbabosco <andrea.magnabosco@antartika.it>
2020-06-01 15:58:58 +0200

    sl05

---
+++ src/Slideshow.js
@@ -0,0 +1,147 @@
+import * as React from "react";
+import styles from "./slideshow.module.css";
+import placeholder from "./img/slide_placeholder.jpg";
+
+function Slideshow({ srcArray }) {
+  let [execIter, setExecIter] = React.useState(0); // This is the number of times that getImg() has run
+  let [position, setPosition] = React.useState(1); // This is the number of the slide that is showed in the carousel
+  let [imgArray, setImgArray] = React.useState([
+    // The component mounts with a lightweight placeholder image
+    <img
+      id='placeholder'
+      src={placeholder}
+      alt='lorem ipsum'
+      key={`deskslide-placeholder}`}
+    />,
+  ]);
+  let [goToSlide, setGoToSlide] = React.useState(null);
+
+  React.useLayoutEffect(() => {
+    function getImg() {
+      // questa funzione serve a caricare le immagini dello slideshow
+      // dopo il suo iniziale caricamento (durante il quale viene caricata
+      // solo un'immagine segnaposto, molto leggera, v. sopra).
+      // Utilizzo questa funzione in modo 🚀 ricorsivo 🚀
+      // (attraverso onLoad: ogni nuova immagine chiama nuovamente
+      // questa funzione per aggiungere un'altra immagine all'array).
+      // La nuova chiamata avviene onLoad, in modo da essere sicuri
+      //  di mostrare un'immagine alla volta nella giusta sequenza.
+      // La condizione di uscita si raggiunge al termine dell'array
+
+      if (execIter === srcArray.length) {
+        // condizione di uscita dalla ricorsione
+        return;
+      }
+
+      if (execIter === 0) {
+        // = se è la prima esecuzione
+        setImgArray([
+          <img
+            id={`slide${execIter + 1}`}
+            src={process.env.PUBLIC_URL + srcArray[0]}
+            alt='lorem ipsum'
+            key={`deskslide${execIter + 1}`}
+          />,
+        ]);
+        setExecIter(execIter + 1);
+      } else {
+        // se è un'esecuzione successiva alla prima
+        setImgArray([
+          ...imgArray,
+          <img
+            id={`slide${execIter + 1}`}
+            src={process.env.PUBLIC_URL + srcArray[execIter]}
+            alt='lorem ipsum'
+            key={`deskslide${execIter + 1}`}
+            onLoad={() => {
+              // questa è la chiamata ricorsiva
+              getImg();
+            }}
+          />,
+        ]);
+        setExecIter(execIter + 1);
+      }
+    }
+
+    getImg();
+  }, [execIter, imgArray, srcArray]);
+
+  React.useLayoutEffect(() => {
+    if (execIter) {
+      // se getImg() è già stato eseguito una volta, c'è la prima img nell'array
+
+      let size = document.getElementById(`slide1`).clientWidth;
+      const carouselSlides = document.getElementById("carouselSlides");
+
+      const myTransition = (xPosition) => {
+        carouselSlides.style.transition = `transform 0.2s ease-in-out`;
+        carouselSlides.style.transform = `translateX(-${xPosition}px)`;
+      };
+      const toSpecificSlide = (index) => {
+        myTransition(index * size);
+        setPosition(index + 1);
+      };
+      setGoToSlide(() => toSpecificSlide);
+    }
+  }, [execIter, setGoToSlide]);
+
+  return (
+    <div>
+      <h1 className={styles.myH1}>React Slideshow</h1>
+      <h2 className={styles.myH2}>
+        Super lightweight, optimized for speed and performance.
+      </h2>
+
+      <div id='carouselContainer' className={styles.container}>
+        <Slides imgArray={imgArray} />
+      </div>
+
+      <Indicators
+        imgArray={imgArray}
+        goToSlide={goToSlide}
+        position={position}
+      />
+    </div>
+  );
+}
+
+export default Slideshow;
+
+function Slides({ imgArray }) {
+  return (
+    <div id='carouselSlides' className={styles.slides}>
+      {imgArray}
+    </div>
+  );
+}
+
+function Indicators({ imgArray, goToSlide, position }) {
+  const prevSlide = () => {
+    if (position > 1) {
+      goToSlide(position - 2);
+    }
+  };
+  const nextSlide = () => {
+    if (position < imgArray.length) {
+      goToSlide(position);
+    }
+  };
+
+  return (
+    <div className={styles.indicators}>
+      <button onClick={() => prevSlide()}>back</button>
+      {imgArray.map((img, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => goToSlide(index)}
+            className={position === index + 1 ? styles.selected : undefined}
+          >
+            {index + 1}
+          </button>
+        );
+      })}
+      <button onClick={() => nextSlide()}>next</button>
+    </div>
+  );
+}
